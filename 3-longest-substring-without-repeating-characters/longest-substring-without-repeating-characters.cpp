8481ff311c9d4c036a8486a2469d1f001af221b5
Tharun kumar <thundert428@gmail.com>
2026-08-10 21:10:30 +0530

    Time: 315 ms (7.06%) | Memory: 81.3 MB (9.73%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> set;
+        int left = 0;
+        int maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            while (set.find(s[right]) != set.end()) {
+                set.erase(s[left]);
+                left++;
+            }
+            set.insert(s[right]);
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};
